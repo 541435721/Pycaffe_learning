f94d7a6ccf6e3b98c47991dd02d83a3707addf25
541435721 <541435721@qq.com>
2016-04-08 10:00:55 +0800

    训练的单层感知机，获得网络模型和参数，并用模型对新数据集进行测试
    Signed-off-by: 541435721 <541435721@qq.com>

---
+++ Hdf5_data_test/guess.py
@@ -0,0 +1,30 @@
+# coding:utf-8
+# __author__ = 'BianXuesheng'
+# __data__ = '2016/04/08_09:27 '
+
+from numpy import *
+import caffe
+from caffe import layers as L
+from caffe import params as P
+import h5py
+import sklearn
+import sklearn.datasets
+import sklearn.linear_model
+
+if __name__ == '__main__':
+    net = caffe.Classifier('deploy.prototxt', 'snap_iter_10000.caffemodel')
+    # net.predict([array([1, 1])])
+    Net = caffe.Net('deploy.prototxt', 'snap_iter_10000.caffemodel', caffe.TEST)
+
+    Net.blobs['data'].data[...] = array([1, 5])
+    out = Net.forward()
+    # out = Net.forward(data=array([1, 5]))
+
+    print 'lebel:' + str(out['prob'][0].argmax())
+
+    t = random.randint(0, 100, (10, 2))
+    for i in t:
+        print i
+        Net.blobs['data'].data[...] = i
+        out = Net.forward()
+        print 'lebel:' + str(out['prob'][0].argmax())
---
+++ Hdf5_data_test/train.py
@@ -0,0 +1,34 @@
+# coding:utf-8
+# __author__ = 'BianXuesheng'
+# __data__ = '2016/04/08_08:52 '
+
+
+from numpy import *
+import caffe
+from caffe import layers as L
+from caffe import params as P
+import h5py
+import sklearn
+import sklearn.datasets
+import sklearn.linear_model
+
+if __name__ == '__main__':
+    x = random.randint(0, 10, (1000, 2))  # 创建1000个样本，特征维度为2
+    y = array(map(lambda x: int(2 * x[0] < x[1]), x))  # 创建标签
+
+    x, xt, y, yt = sklearn.cross_validation.train_test_split(x, y)  # 分割训练集和数据集
+
+    with h5py.File('train_data.h5', 'w') as f:
+        f['data'] = x
+        f['label'] = y.astype(float32)
+    with open('train.txt', 'w') as f:
+        f.write('train_data.h5' + '\n')
+
+    with h5py.File('test_data.h5', 'w') as f:
+        f['data'] = xt
+        f['label'] = yt.astype(float32)
+    with open('test.txt', 'w') as f:
+        f.write('test_data.h5' + '\n')
+
+    solver = caffe.get_solver('solver.prototxt')  # 设置优化器配置文件
+    solver.solve()
